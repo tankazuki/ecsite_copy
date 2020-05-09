7cf29a6be38902e0b1558ff5afa6170934a311d2
tankazuki <34229874+tankazuki@users.noreply.github.com>
2020-05-10 05:04:34 +0900

    UserのログインFormを追加

---
+++ amazon/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+
+class LoginForm(AuthenticationForm):
+    """ログインフォーム"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label
